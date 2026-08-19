502ff05bb747310e2a16ba5c6897ad8633880822
kent <kent.26pg@gmail.com>
2026-08-19 12:58:43 +0200

    Added Gutendex API fetch to Books page

--- vite-project/src/Pages/BookDetails.jsx
+++ vite-project/src/Pages/BookDetails.jsx
@@ -1,7 +1,11 @@
+import { useParams } from "react-router-dom";
+
 function BookDetails() {
+    const { id } = useParams()
   return (
     <div>
       <h1>Book Details</h1>
+      <p>Book ID: {id}</p>
     </div>
   );
 }
--- vite-project/src/Pages/Books.jsx
+++ vite-project/src/Pages/Books.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
+
 function Books() {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("https://gutendex.com/books");
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        console.log(data);
+
+        setBooks(data.results);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <h1>Books</h1>
-      <p>Her kommer bøkene.</p>
+
+      {books.map((book) => (
+        <p key={book.id}>{book.title}</p>
+      ))}
     </div>
   );
 }
