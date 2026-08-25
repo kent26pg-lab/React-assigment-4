50c4ac658dd694332010266925a95d980c9076c1
kent <kent.26pg@gmail.com>
2026-08-25 11:41:48 +0200

    added styling to the homepage

--- vite-project/src/Pages/Home.jsx
+++ vite-project/src/Pages/Home.jsx
@@ -1,14 +1,32 @@
+import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 
 function Home() {
   return (
-    <main className={styles.container}>
-      <h1 className={styles.title}>Welcome to the Bookstore</h1>
+    <section className={styles.home}>
+      <div className={styles.cover}>
+        <div className={styles.decorativeLine}></div>
 
-      <p className={styles.text}>
-        Her kan du utforske bøker fra Gutendex.
-      </p>
-    </main>
+        <p className={styles.smallText}>THE BOOKSHELF</p>
+
+        <h1>Discover Your<br />Next Book</h1>
+
+        <p className={styles.description}>
+          Explore thousands of free books from Project Gutenberg.
+          Search, discover and save your favorites.
+        </p>
+
+        <Link to="/books" className={styles.button}>
+          Explore Books
+        </Link>
+
+        <div className={styles.decorativeLine}></div>
+
+        <p className={styles.footerText}>
+          READ • DISCOVER • ENJOY
+        </p>
+      </div>
+    </section>
   );
 }
 
